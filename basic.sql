13704416e1e2493a85302d3a28f34c753fd35954
denokenya <dennismuteti25@gmail.com>
2022-03-14 12:16:22 +0300

    using months_between function

--- basic.sql
+++ basic.sql
@@ -6,3 +6,11 @@ FROM salgrade
 
 SELECT ename , deptno , sal , RANK() OVER(ORDER BY sal DESC) AS rank  ,DENSE_RANK() OVER(ORDER BY sal DESC) AS dense_rank
 FROM emp
+
+------------------------------------------------------
+--Calculate the months between hiredate and the current date order by employee name
+SELECT ename,sal * 12,TRUNC(MONTHS_BETWEEN(SYSDATE, hiredate)) 
+
+FROM emp
+WHERE deptno = 30
+ORDER BY ename;
